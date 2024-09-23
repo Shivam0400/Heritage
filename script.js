ffa935bc7cce8046a8a2f1293f0828ab14262fe2
Shivam Jaiswal <153131745+Shivam0400@users.noreply.github.com>
2024-09-23 16:01:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Function to simulate starting a virtual tour
+function startTour(tour) {
+    alert('Starting ' + tour + ' virtual tour.');
+}
+
+// Function to simulate starting a cultural course
+function startCourse(course) {
+    alert('Starting ' + course + ' course.');
+}
+
+// Function to simulate buying a product
+function buyProduct(product) {
+    alert('You have selected to buy ' + product + '.');
+}
